f7c558f21768ebec17497fab4c47d177f5e90490
David Nogue Cuadrado <davidnogue08@gmail.com>
2020-04-08 20:27:31 +0200

    PartidaUsuario UnitTest

---
+++ Muim.Data/Contracts/IUserPartida.cs
@@ -0,0 +1,14 @@
+﻿
+using Muim.Domain.Models;
+using System.Collections.Generic;
+
+namespace Muim.Data.Contracts
+{
+    public interface IUserPartida
+    {
+        bool AddUserPartida(PartidaUsuario partidauser);
+        bool DeleteUser(int id);
+        List<PartidaUsuario> GetPartidaUsuario();
+
+    }
+}
---
+++ Muim.Data/Implemention/UserPartida.cs
@@ -0,0 +1,30 @@
+﻿
+using Muim.Data.Contracts;
+using Muim.Domain.Models;
+using System.Collections.Generic;
+
+namespace Muim.Data.Implemention
+{
+    public class UserPartida : IUserPartida
+    {
+        public bool AddUserPartida(PartidaUsuario partidauser)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DeleteUser(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<PartidaUsuario> GetPartidaUsuario()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateUser(PartidaUsuario partidauser)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
